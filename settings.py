2bd525f9bdc0c75b757b26175b67bf3e67f2f22a
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-06-26 21:56:17 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,178 @@
+import os, pathlib
+import streamlit as st
+import os, datetime, json, sys, pathlib, shutil
+import pandas as pd
+import streamlit as st
+import cv2
+import face_recognition
+import numpy as np
+from PIL import Image
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LOG_CONFIG = os.path.join(ROOT_DIR, 'logging.yml')
+
+STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
+t
+DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
+if not DOWNLOADS_PATH.is_dir():
+    DOWNLOADS_PATH.mkdir()
+
+LOG_DIR = (STREAMLIT_STATIC_PATH / "logs")
+if not LOG_DIR.is_dir():
+    LOG_DIR.mkdir()
+
+OUT_DIR = (STREAMLIT_STATIC_PATH / "output")
+if not OUT_DIR.is_dir():
+    OUT_DIR.mkdir()
+
+VISITOR_DB = os.path.join(ROOT_DIR, "visitor_database")
+
+if not os.path.exists(VISITOR_DB):
+    os.mkdir(VISITOR_DB)
+
+VISITOR_HISTORY = os.path.join(ROOT_DIR, "visitor_history")
+
+if not os.path.exists(VISITOR_HISTORY):
+    os.mkdir(VISITOR_HISTORY)
+
+COLOR_DARK  = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+COLS_INFO   = ['Name']
+COLS_ENCODE = [f'v{i}' for i in range(128)]
+
+data_path       = VISITOR_DB
+file_db         = 'visitors_db.csv'         
+file_history    = 'visitors_history.csv'    
+
+
+allowed_image_type = ['.png', 'jpg', '.jpeg']
+
+def initialize_data():
+    if os.path.exists(os.path.join(data_path, file_db)):
+        
+        df = pd.read_csv(os.path.join(data_path, file_db))
+
+    else:
+        
+        df = pd.DataFrame(columns=COLS_INFO + COLS_ENCODE)
+        df.to_csv(os.path.join(data_path, file_db), index=False)
+
+    return df
+
+
+def add_data_db(df_visitor_details):
+    try:
+        df_all = pd.read_csv(os.path.join(data_path, file_db))
+
+        if not df_all.empty:
+            df_all = df_all.append(df_visitor_details, ignore_index=False)
+            df_all.drop_duplicates(keep='first', inplace=True)
+            df_all.reset_index(inplace=True, drop=True)
+            df_all.to_csv(os.path.join(data_path, file_db), index=False)
+            st.markdown('''
+            <style>
+            .st-b7 {
+                color: #cbe4de;
+                background-color: #0e8388;
+                align-text: center;
+            }
+            </style>
+            ''', unsafe_allow_html=True)
+            st.success('Details Added Successfully!')
+        else:
+            df_visitor_details.to_csv(os.path.join(data_path, file_db), index=False)
+            st.markdown('''
+            <style>
+            .st-b7 {
+                color: #cbe4de;
+                background-color: #0e8388;
+                align-text: center;
+            }
+            </style>
+            ''', unsafe_allow_html=True)
+            st.success('Initiated Data Successfully!')
+
+    except Exception as e:
+        st.error(e)
+
+
+def BGR_to_RGB(image_in_array):
+    return cv2.cvtColor(image_in_array, cv2.COLOR_BGR2RGB)
+
+
+def findEncodings(images):
+    encode_list = []
+
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encode_list.append(encode)
+
+    return encode_list
+
+
+def face_distance_to_conf(face_distance, face_match_threshold=0.6):
+    if face_distance > face_match_threshold:
+        range = (1.0 - face_match_threshold)
+        linear_val = (1.0 - face_distance) / (range * 2.0)
+        return linear_val
+    else:
+        range = face_match_threshold
+        linear_val = 1.0 - (face_distance / (range * 2.0))
+        return linear_val + ((1.0 - linear_val) * np.power(
+            (linear_val - 0.5) * 2, 0.2))
+
+
+def attendance(id, name):
+    f_p = os.path.join(VISITOR_HISTORY, file_history)
+    
+
+    now = datetime.datetime.now()
+    dtString = now.strftime('%Y-%m-%d %H:%M:%S')
+    df_attendace_temp = pd.DataFrame(data={ "id"            : [id],
+                                            "visitor_name"  : [name],
+                                            "Timing"        : [dtString]
+                                            })
+
+    if not os.path.isfile(f_p):
+        df_attendace_temp.to_csv(f_p, index=False)
+        
+    else:
+        df_attendace = pd.read_csv(f_p)
+        df_attendace = df_attendace.append(df_attendace_temp)
+        df_attendace.to_csv(f_p, index=False)
+        
+def view_attendace():
+    f_p = os.path.join(VISITOR_HISTORY, file_history)
+    
+    df_attendace_temp = pd.DataFrame(columns=["id",
+                                              "visitor_name", "Timing"])
+
+    if not os.path.isfile(f_p):
+        df_attendace_temp.to_csv(f_p, index=False)
+    else:
+        df_attendace_temp = pd.read_csv(f_p)
+
+    df_attendace = df_attendace_temp.sort_values(by='Timing',
+                                                 ascending=False)
+    df_attendace.reset_index(inplace=True, drop=True)
+
+    st.write(df_attendace)
+
+    if df_attendace.shape[0]>0:
+        id_chk  = df_attendace.loc[0, 'id']
+        id_name = df_attendace.loc[0, 'visitor_name']
+
+        selected_img = st.selectbox('Search Image using ID',
+                                    options=['None']+list(df_attendace['id']))
+
+        avail_files = [file for file in list(os.listdir(VISITOR_HISTORY))
+                       if ((file.endswith(tuple(allowed_image_type))) &
+                                                                              (file.startswith(selected_img) == True))]
+
+        if len(avail_files)>0:
+            selected_img_path = os.path.join(VISITOR_HISTORY,
+                                             avail_files[0])
+            
+            st.image(Image.open(selected_img_path))
